92e83cf3239e0ef9dd4815d52e478ca1e8f48cc0
onore_gleb <onoreg@mail.ru>
2025-03-17 13:25:25 +0300

    add docstrings

--- image_processing.py
+++ image_processing.py
@@ -3,6 +3,14 @@ from PIL import Image
 import io
 
 def remove_background(image: bytes):
+    """
+    Удаляет фон с изображения
+
+    :param image: Входное изображение
+    :type image: bytes
+    :return: Обработанное изображение с удаленным фоном
+    :rtype: PIL.Image
+    """
     input_image = Image.open(image).convert("RGBA")
     output_image = remove(input_image)
 
@@ -10,12 +18,20 @@ def remove_background(image: bytes):
 
 
 def replace_background(photo_bytes: bytes):
-    background = Image.open("backgrounds/ex1.png")
+    """
+    Заменяет фон на загруженном изображении (с использованием шаблона из backgrounds/)
+    :param photo_bytes: Изображение, на котором нужно заменить фон
+    :type photo_bytes: bytes
+    :return: Изображение с замененным фоном
+    :rtype: PIL.Image
+    """
+    background = Image.open("backgrounds/ex1.png").convert("RGBA")
     image = Image.open(io.BytesIO(photo_bytes)).convert("RGBA")
 
     width, height = image.size
-    new_background = Image.new("RGBA", (width, height), background)
+    background = background.resize((width, height))
 
+    new_background = background.copy()
     new_background.paste(image, (0, 0), image)
 
     return new_background
